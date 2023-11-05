7cc76b49ff259f80e8e3576bd410741644a55e5a
sachin <scnmrsn9818@gmail.com>
2023-11-05 14:41:10 +0545

    second

---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className='container d-flex justify-content-center' style={{height: "98vh"}}>
+      <img className='' src='/images/XOsX.gif' alt='Unable to load'></img>
+    </div>
+  )
+}
---
+++ src/components/Navbr.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "../stiles/navbr.css"
+import { Link } from 'react-router-dom'
+
+export default function Navbr() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">NewZ</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">About</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Category
+                            </Link>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item" to="/business">business</Link></li>
+                                <li><Link className="dropdown-item" to="/entertainment">entertainment</Link></li>
+                                <li><Link className="dropdown-item" to="/food">food</Link></li>
+                                <li><Link className="dropdown-item" to="/health">health</Link></li>
+                                <li><Link className="dropdown-item" to="/science">science</Link></li>
+                                <li><Link className="dropdown-item" to="/sports">sports</Link></li>
+                                <li><Link className="dropdown-item" to="/technology">technology</Link></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
