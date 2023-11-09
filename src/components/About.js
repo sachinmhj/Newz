aa646122ff5142cd9c36921b2bffb9299d6459d6
sachin mhj <scnmrsn9818@gmail.com>
2023-11-09 19:30:57 +0545

    compl

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <>
-            <div style={{"padding": "100px","text-align": "center","background-color": "#474e5d","color": "white"}}>
+            <div style={{"padding": "100px","textAlign": "center","backgroundColor": "#474e5d","color": "white"}}>
                 <h1><b>About Us</b></h1>
                 <p>Welcome to NewZ - Your Trusted Source for Unbiased News!</p>
                 <p>At NewZ, we are committed to delivering accurate and reliable news from around the world. Our mission is to keep you informed, enlightened, and empowered with the latest happenings across various categories. NewZ is a dedicated team of journalists, editors, and news enthusiasts passionate about bringing you timely and objective information. With a commitment to journalistic integrity, we strive to provide news stories that matter to you.</p>
